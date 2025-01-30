cf3391dc49b3d4b52bc7f749220d38488abd6524
ikabeee <carlglz30@gmail.com>
2025-01-30 04:10:44 -0500

    Add heroui components

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@heroui/theme/dist/components/(alert|button|form|input|spinner|ripple).js"
+    "./node_modules/@heroui/theme/dist/components/(alert|button|dropdown|form|input|spinner|ripple|menu|divider|popover).js"
   ],
   theme: {
     extend: {},
