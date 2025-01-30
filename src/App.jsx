1530ebfa8fa425cb4db3506caf5376b30c709816
ikabeee <carlglz30@gmail.com>
2025-01-29 22:27:28 -0500

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from 'react-router';
+import Login from './pages/Login';
+import Forbidden from './pages/errors/Forbidden';
 function App() {
 
   return (
     <>
+    <Routes>
+      <Route path='/error/forbidden' element={<Forbidden />}/>
+      <Route path='/login' element={<Login/>} />
+    </Routes>
+
     </>
   )
 }
