0037cbf5f21b64c81d8c49db9395342cd8bd3430
SebasEPV <sperezvinas@gmaill.com>
2025-01-29 21:17:10 -0500

    Added Dashboard for Members or Collaborators

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import { Navbar } from './layout/Navbar'
-import { Sidebar } from './layout/Sidebar'
 import { DashboardMember } from './pages/DashboardMember'
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar />
       <div className="flex flex-1">
-        <Sidebar />
         <DashboardMember />
       </div>
     </div>
